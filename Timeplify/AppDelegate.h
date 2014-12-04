dc20c725b59e7fe6f6cbee983e53ea74da81cb25
Anil <anil@Anils-Mac-Pro.local>
2014-12-04 09:11:21 +0530

    Initial Commit

---
+++ Timeplify/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Timeplify
+//
+//  Created by Anil on 04/12/14.
+//  Copyright (c) 2014 Anil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Timeplify/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Timeplify
+//
+//  Created by Anil on 04/12/14.
+//  Copyright (c) 2014 Anil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
